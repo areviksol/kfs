bb63850d00b86d7b54d330ef74d1b953585f2fb0
Arevik Mkrtchyan <arevik.mkrtchian1997@gmail.com>
2025-12-28 17:39:30 +0400

    🐓

--- kfs-1/types.h
+++ kfs-1/types.h
@@ -4,8 +4,8 @@
 #include <stdint.h>  // Предоставляет uint8_t, uint16_t, uint32_t, uint64_t и т. п.
 
 // Определяем size_t только если компилятор не предоставил его сам
-#ifndef __SIZE_TYPE__
-typedef uint32_t size_t;  // Для 32-битного ядра size_t — 32-битное беззнаковое целое
+#ifndef __cplusplus
+typedef uint32_t size_t;
 #endif
 
 #endif // TYPES_H
